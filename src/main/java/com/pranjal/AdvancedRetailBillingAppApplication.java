0de7d5f1d0db63981963417a3bb8ba52e5d5f099
Pranjal Jais <pranjaljais2@gmail.com>
2026-06-15 12:20:43 +0530

    Initial commit

---
+++ src/main/java/com/pranjal/AdvancedRetailBillingAppApplication.java
@@ -0,0 +1,13 @@
+package com.pranjal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedRetailBillingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedRetailBillingAppApplication.class, args);
+	}
+
+}
